2a7059a5bb9a36301633b2dd9e09c65a6b481e54
pomoranca <armin_x@hotmail.com>
2020-08-10 15:21:22 +0200

    Initial commit

---
+++ app/src/main/java/com/coffeetime/supplementshop/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.coffeetime.supplementshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val host = supportFragmentManager
+            .findFragmentById(R.id.homeFragment) as NavHostFragment
+        val navController = host.navController
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
+    }
+}
